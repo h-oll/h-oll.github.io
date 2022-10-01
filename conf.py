f05d5fd8bc956f42bdc27b133b826f85f54bfc9e
holl@mercury <harold.ollivier@inria.fr>
2022-10-01 21:16:25 +0200

    Nikola auto commit.
    Nikola version: 8.2.3

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Harold Ollivier"  # (translatable)
-BLOG_TITLE = "Harold Ollivier"  # (translatable)
+BLOG_TITLE = "Ollivier Group"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://h-oll.github.io/"
